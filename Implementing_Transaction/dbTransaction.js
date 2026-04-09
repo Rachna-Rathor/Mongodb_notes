6aade56593d9f29190c576710f836cd800bd6304
Rachna-Rathor <rathorrachna679@gmail.com>
2026-04-09 23:23:26 +0530

    add schema for controller user

--- Implementing_Transaction/dbTransaction.js
+++ Implementing_Transaction/dbTransaction.js
@@ -2,23 +2,40 @@ const { MongoClient } = require("mongodb");
 let db;
 
 async function run() {
-  const client = new MongoClient("mongodb://localhost:27017");
-  await client.connect();
-  console.log("database connected");
+    const client = new MongoClient("mongodb://localhost:27017");
+    await client.connect();
+    console.log("database connected");
 
-  db = client.db("test");
+    db = client.db("test");
 
-  const directories = db.collection("directories");
-  const user = db.collection("user");
+    const directories = db.collection("directories");
+    const user = db.collection("user");
+    const session = client.startSession()
+    session.startTransaction()
+    try {
+        await directories.insertOne({ name: "db", userName: "Anurag" });
+        await user.insertOne({ name: "Rachna", age: 21 });
+        session.commitTransaction();
+        console.log("data successfully inserted"); // ✅ instead of res.send
 
-  await directories.insertOne({ name: "db", userName: "Anurag" });
-  await user.insertOne({ name: "Rachna", age: 21 });
-
-  console.log("data successfully inserted"); // ✅ instead of res.send
-
-  await client.close();
+    } catch (error) {
+await session.abortTransaction()
+    }
+    await client.close();
 }
 
 module.exports = {
-  run,
+    run,
 };
+
+
+// What is a Session?
+// A session in MongoDB is a logical context that groups multiple operations together.
+// It allows MongoDB to track and manage operations as part of transactions or causal consistency.
+// Think of a session as a workspace where you can perform one or more transactions safely.
+// Why Sessions Are Used
+// 1 : To start transactions (for atomic multi-document or multi-collection operations).
+
+// 2: To maintain causal consistency (operations happen in a guaranteed order).
+
+// 3: To logically group related operations together under one context.
